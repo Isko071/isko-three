a649793fb6ce599ac6d898a59f70eae3bcb015e0
Isko071 <izluk200@mail.ru>
2026-05-09 00:07:36 +0600

    content

--- secret.js
+++ secret.js
@@ -9,3 +9,18 @@ if (hasToken === true && (page.includes("index.html") || page === "/")) {
 if (hasToken === false && page.includes("secret.html")) {
     window.location.href = "/index.html";
 }
+
+// 1. Находим кнопку в коде
+document.querySelector("#logout-btn").addEventListener("click", (event) => {
+    event.preventDefault();
+    
+    // 3. Удаляем куку "token"
+    // Мы просто ставим время жизни куки (max-age) на 0 секунд.
+    document.cookie = "token=; max-age=0; path=/";
+
+    // 4. Вызываем твою функцию, которая переключает страницы
+    // Она увидит, что токена больше нет, и покажет форму входа.
+    showPage(); 
+    
+    console.log("Вы вышли из системы");
+});
