060b13f61dee3feb4283c62611f2f906bd17f945
Isko071 <izluk200@mail.ru>
2026-05-12 12:17:26 +0600

    carousel

--- movie/secret.js
+++ movie/secret.js
@@ -24,3 +24,50 @@ document.querySelector("#logout-btn").addEventListener("click", (event) => {
     
     console.log("Вы вышли из системы");
 });
+
+
+const track = document.querySelector('.carousel-track');
+const nextBtn = document.querySelector('#next-button');
+const prevBtn = document.querySelector('#prev-button');
+let index = 0; // Номер текущей страницы (0 или 1)
+
+function updateCarousel() {
+    const width = document.querySelector('.carousel-window').clientWidth + 15;
+    track.style.transition = "transform 0.5s ease-in-out"; // Включаем анимацию
+    track.style.transform = `translateX(${-index * width}px)`;
+}
+
+// Слушатель для кнопки "Вперед"
+nextBtn.addEventListener('click', () => {
+    index++;
+    updateCarousel();
+
+    // Если мы дошли до клонов (index 2)
+    if (index === 2) {
+        setTimeout(() => {
+            track.style.transition = "none"; // ВЫКЛЮЧАЕМ анимацию
+            index = 0;                       // Сбрасываем индекс на начало
+            track.style.transform = `translateX(0)`; 
+        }, 500); // Ждем ровно 0.5с (время анимации), чтобы прыгнуть
+    }
+});
+
+// Слушатель для кнопки "Назад"
+prevBtn.addEventListener('click', () => {
+    if (index === 0) {
+        // Если мы в начале, мгновенно прыгаем на клонов в конце
+        track.style.transition = "none";
+        index = 2;
+        const width = document.querySelector('.carousel-window').clientWidth + 15;
+        track.style.transform = `translateX(${-index * width}px)`;
+        
+        // И сразу заставляем браузер плавно поехать на страницу 1
+        setTimeout(() => {
+            index = 1;
+            updateCarousel();
+        }, 10);
+    } else {
+        index--;
+        updateCarousel();
+    }
+});
